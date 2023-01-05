4b1b7012a50ff735141856d207f2bcfa2554afaa
genie-openj9 <openj9-bot@eclipse.org>
2023-01-05 09:08:35 +0000

    Updates

--- docs/javascripts/oj9.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-
-    if (document.getElementById("api") !== null) {
-        document.getElementsByClassName('md-sidebar--secondary')[0].style.position="absolute";
-        document.getElementsByClassName('md-sidebar--secondary')[0].style.left="-10000px";
-    }
-}
